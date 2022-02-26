802b0981502e1a7bbf24150d500e7a6415690b8e
Andrey <sazonov31294@gmail.com>
2022-02-27 02:28:08 +0300

    Возврат строки об ошибки, Linter не принимает console.error и alert

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 function getRandomInteger(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  if ((min && max >= 0) && (min && max !== String) && max > min) {
+  if (min >= 0 && max >= 0 && max > min && min !== String && max !== String) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return console.error('Исходные данные неверны');
+  return 'Исходные данные неверны';
 }
 
 function getRandomWithDot(min, max, digits) {
-  if ((min && max >= 0) && (min && max !== String) && max > min) {
+  if (min >= 0 && max >= 0 && max > min && min !== String && max !== String) {
     return ((Math.random() * (max - min)) + min).toFixed(digits);
   }
-  return console.error('Исходные данные неверны');
+  return 'Исходные данные неверны';
 }
 
-getRandomInteger(1, 19);
+getRandomInteger(0, 19);
 getRandomWithDot(0.123456789, 10.123456789, 3);
+
+
