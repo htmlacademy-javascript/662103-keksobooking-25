a163692efc6e381e52cef5db82fda7a0e5ad86d8
Andrey <sazonov31294@gmail.com>
2022-03-15 18:52:41 +0300

    Реализация активного/неактивного состояния

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import './utils.js';
 import {cardsOffers} from './create-new-offer.js';
 import './generating-markup.js';
+import './active-inactive-state.js';
 // eslint-disable-next-line no-console
 console.log(cardsOffers);
